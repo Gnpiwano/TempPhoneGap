9c4733d8457708a91079232ace50c42e71161df7
gnpiwano <gnpiwano@student.avans.nl>
2016-03-12 16:09:08 +0100

    no message

--- www/app/shared/shared-service.js
+++ www/app/shared/shared-service.js
@@ -7,3 +7,25 @@ angular.module("ngapp").service("shared", function(){ // One of The Ways To Shar
         auth: "Mario Aleo"
     };
 });
+
+angular.factory('dataService', function() {
+    var savedData = {}
+    function set(data) {
+        savedData = data;
+    }
+    function get() {
+        return savedData;
+    }
+
+    return {
+        set: set,
+        get: get
+    }
+});
+
+angular.factory('PokemonService', function ($resource) {
+
+    //
+    var data
+    return $resource('http://pokeapi.co/api/v2/pokemon/:id/?limit=20');
+});
