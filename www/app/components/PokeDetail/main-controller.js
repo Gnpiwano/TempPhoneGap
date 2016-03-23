2cab6a1379533e80fe0057c41ec1b5dee8c64e34
Guus Iwanow <Guusiwanow@hotmail.coml>
2016-03-23 10:58:48 +0100

    no message

--- www/app/components/PokeDetail/main-controller.js
+++ www/app/components/PokeDetail/main-controller.js
@@ -4,22 +4,18 @@ angular.module("ngapp").controller("PokeDetailController", function(shared,Pokem
 
     $scope.pokemon = shared.currentPokemon;
 
+    alert("First alert test");
     $scope.init = function() {
         var tempPokemon = $resource("http://pokeapi.co/api/v2/pokemon/"+shared.currentPokemon.id+"/").get();
+        alert("second alert test");
+
+        document.addEventListener("backbutton", function (e) {
+            alert("Test Go Back Button");
+        }, false);
 
         tempPokemon.$promise.then(function(data) {
             $scope.pokemon = data;
             //console.log("Resource" , data);
-            document.addEventListener("backbutton", function (e) {
-                alert("Test Go Back Button");
-
-                //if($state.is('detail')){
-                //  location.replace("#/main");
-                //}  else{
-                //  e.preventDefault();
-                //}
-
-            }, false);
         });
     }
 
