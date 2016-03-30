8c4987a4f9c71e61f3d7e5d3b65dd274b66b129d
Guus Iwanow <Guusiwanow@hotmail.coml>
2016-03-30 12:41:22 +0200

    no message

---
+++ platforms/browser/www/app/components/loadingScreen/loading_controller.js
@@ -0,0 +1,14 @@
+"use strict";
+
+angular.module("ngapp").controller("loadingController", function(SettingsMenu, shared,PokemonService,$resource,$state, $scope, $mdSidenav, $mdComponentRegistry, $location){
+
+    $scope.loading_gif = "assets/loading.gif";
+    $scope.menu = SettingsMenu;
+
+    PokemonService.checkForUpdates();
+
+    document.addEventListener("pokedex_ready", function(e) {
+        location.replace("#/main");
+    });
+
+});
